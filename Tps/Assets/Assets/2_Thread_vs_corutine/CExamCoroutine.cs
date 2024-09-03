64b4e238d52573d1c14be113cadaac8f95f3ce6a
gf01\User <sbsgameas@gmail.com>
2024-09-03 19:50:38 +0900

    no message

--- Tps/Assets/Assets/2_Thread_vs_corutine/CExamCoroutine.cs
+++ Tps/Assets/Assets/2_Thread_vs_corutine/CExamCoroutine.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-System.Collections.IEnumerator mDoDispatch = null;
 public class CExamCoroutine : MonoBehaviour
 {
 
